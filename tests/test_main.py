d40745753f2ff43016726a67cb30bb274d0fc349
Rostyslav Misiura <rostislav9999@gmail.com>
2021-05-06 16:43:36 +0300

    Update tests added coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,15 +2,66 @@ import pytest
 
 
 def test_configer(monkeypatch):
+    from pyconfiger import Configer
     from pyconfiger import ConfigError
-    from pyconfiger import configer
+    configer = Configer()
 
     monkeypatch.setenv('TEST_ENV', 'TEST_ENV_VALUE')
     assert configer.get('TEST_ENV') == 'TEST_ENV_VALUE'
     with pytest.raises(ConfigError):
         configer.get('TEST_ENV_NOT_EXIST')
+    assert configer.get('TEST_ENV_NOT_EXIST', 'default_value') == 'default_value'
 
 
-def test_configer_context():
-    from pyconfiger import configer
-    assert configer.get('TEST_ENV') == 'TEST_ENV_VALUE'
+def test_keybuilder(monkeypatch):
+    from pyconfiger import Configer
+    from pyconfiger import key_builder
+    configer = Configer()
+
+    @key_builder(use=(
+        'KEY1',
+        'KEY2',
+    ))
+    def built_key(key1, key2):
+        return f'{key1}+{key2}'
+
+    monkeypatch.setenv('KEY1', 'VALUE1')
+    monkeypatch.setenv('KEY2', 'VALUE2')
+
+    configer.built_keys['BUILT_KEY'] = built_key
+    assert configer.get('BUILT_KEY') == 'VALUE1+VALUE2'
+
+
+def test_broken_keybuilder():
+    from pyconfiger import Configer
+    from pyconfiger import ConfigError
+    from pyconfiger import key_builder
+
+    # empty key in use
+    configer = Configer()
+    @key_builder(use=(
+        'KEY1',
+        'KEY2',
+    ))
+    def built_key(key1, key2):
+        return f'{key1}+{key2}'
+    configer.built_keys['BUILT_KEY'] = built_key
+    with pytest.raises(ConfigError):
+        configer.get('HOME')
+
+    #borken use
+    configer = Configer()
+    with pytest.raises(ValueError):
+        @key_builder(use=1)
+        def built_key(key1):
+            return f'{key1}'
+
+
+def test_required():
+    from pyconfiger import Configer
+    from pyconfiger import ConfigError
+    configer = Configer()
+
+    configer.required_keys = ('KEY',)
+    with pytest.raises(ConfigError):
+        configer.get('KEY')

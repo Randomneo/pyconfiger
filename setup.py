3885998776685c290e3fcb7ee3807b5a83ef0896
Rostyslav Misiura <rostislav9999@gmail.com>
2021-05-06 15:29:55 +0300

    Added isort and updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyconfiger',
-    version='0.1.0',
+    version='0.1.1',
     packages=['pyconfiger'],
     author='Rostyslav Misiura',
     author_email='rostislav9999@gmail.com',
